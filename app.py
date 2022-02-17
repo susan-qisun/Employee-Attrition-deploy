b077c29bb631a6a082948152b6bc3037140ccc2a
Susan Qi Sun <99861515+susan-qisun@users.noreply.github.com>
2022-02-17 15:08:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask,render_template,request
 import numpy as np
 import pickle
 
-model=pickle.load(open('Healthcare.pkl','rb'))
+model=pickle.load(open('Employee_attrition.pkl','rb'))
+scaler = pickle.load(open('scaler.pkl', 'rb'))
 
 app = Flask(__name__)
 
@@ -22,14 +23,20 @@ def predict():
     data8=float(request.form['h'])
     data9=float(request.form['i'])
     data10=float(request.form['j'])
-    features=np.array([data1,data2,data3,data4,data5,data6,data7,data8,data9,data10])
-    pred = model.predict([features])
+    data11=float(request.form['k'])
+    dat12=float(request.form['l'])
+    data13=float(request.form['m'])
+    data14=float(request.form['n'])
+    data15=float(request.form['o'])  
+    features=np.array([data1,data2,data3,data4,data5,data6,data7,data8,data9,data10,data11,data12,data13,data14,data15])
+    test_scaled_set = scaler.transform([features])
+    pred = model.predict(test_scaled_set)
     
     def statement():
         if pred == 0:
-            return 'Result:- The model has predicted that you will not suffer from any cardic arresst but you should take care of your self.'
+            return 'Result:- The model has predicted that your employee is not inclined to leave your company.'
         elif pred == 1:
-            return 'Result:- You should consult with doctor, The model has predicted that you will suffer form cardic arrest.'
+            return 'Result:- Your employee will leave your company!'
     
     return render_template('new.html',statement=statement())
 
